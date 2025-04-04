43e45575979f448cfd7b4c5ac7715b722a677aa3
Thedeletebutton <ANDREWSTEVENKEIM@GMAIL.COM>
2025-04-04 13:36:16 -0400

    Add files via upload

---
+++ curated-environments-fixed-socials/src/ThankYou.js
@@ -0,0 +1,9 @@
+export default function ThankYou() {
+  return (
+    <main className='min-h-screen flex flex-col items-center justify-center p-10 text-center'>
+      <h1 className='text-4xl font-bold mb-4'>Thank You!</h1>
+      <p className='text-lg mb-6'>We've received your message. We'll be in touch soon to discuss your space!</p>
+      <a href='/' className='text-blue-600 underline'>← Back to Home</a>
+    </main>
+  );
+}
